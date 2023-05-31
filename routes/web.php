c2931a5dda0927da0d639f60f7791a4fd9e20060
Jadis Jale <jadisjale@gmail.com>
2023-05-31 15:41:57 -0300

    feat: add middleware auth

--- routes/web.php
+++ routes/web.php
@@ -19,17 +19,21 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::post('/auth', [UserController::class, 'auth'])->name("auth.user");
 
 //view
 Route::get('/', [UserController::class, 'index']);
 
-Route::get('/contact', [ContactController::class, 'newContactView']);
 Route::get('/contacts', [ContactController::class, 'index']);
-Route::get('/contact/{id}', [ContactController::class, 'updateView']);
-Route::get('/contact/detail/{id}', [ContactController::class, 'showView']);
 
 //actions
-Route::post('/contact', [ContactController::class, 'store'])->name("contact.store");
-Route::patch('/contact', [ContactController::class, 'update'])->name("contact.update");
-Route::delete('/contact/{id}', [ContactController::class, 'delete'])->name('contact.destroy');
+Route::post('/auth', [UserController::class, 'auth'])->name("auth.user");
+Route::get('/logout', [UserController::class, 'logout'])->name("auth.logout");
+
+Route::middleware('auth.session')->group(function () {
+    Route::get('/contact', [ContactController::class, 'newContactView']);
+    Route::get('/contact/{id}', [ContactController::class, 'updateView']);
+    Route::get('/contact/detail/{id}', [ContactController::class, 'showView']);
+    Route::post('/contact', [ContactController::class, 'store'])->name("contact.store");
+    Route::patch('/contact', [ContactController::class, 'update'])->name("contact.update");
+    Route::delete('/contact/{id}', [ContactController::class, 'delete'])->name('contact.destroy');
+});
