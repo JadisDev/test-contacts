e35b62adbe30c231ef7b98df70ca83706a51826a
Jadis Jale <jadisjale@gmail.com>
2023-05-31 14:35:27 -0300

    feat: init datatable

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -14,3 +14,9 @@
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
 <script src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js"></script>
+
+<script>
+    $(document).ready(function() {
+        $('#table').DataTable();
+    });
+</script>
