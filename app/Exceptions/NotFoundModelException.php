f42acb0f5c87109fbd1f75210fc22ba888678ee6
Jadis Jale <jadisjale@gmail.com>
2023-05-31 14:23:46 -0300

    refactor: added typing

--- app/Exceptions/NotFoundModelException.php
+++ app/Exceptions/NotFoundModelException.php
@@ -7,7 +7,7 @@ use Throwable;
 
 class NotFoundModelException extends Exception
 {
-    public function __construct($message = "model not found", $code = 0, Throwable $previous = null) {
+    public function __construct(string $message = "model not found", int $code = 0, Throwable $previous = null) {
         parent::__construct($message, $code, $previous);
     }
 }
