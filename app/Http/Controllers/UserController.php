0d27d2d0a9e8442535ed543547f6238beb4be04b
Jadis Jale <jadisjale@gmail.com>
2023-05-31 15:43:07 -0300

    feat: router logout

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Request;
+
 class UserController extends Controller
 {
     public function index()
@@ -9,8 +11,15 @@ class UserController extends Controller
         return view('pages.auth.login');
     }
 
-    public function auth()
+    public function auth(Request $request)
     {
+        $request->session()->put('login', 'active');
         return redirect('/contacts');
+    }
+
+    public function logout(Request $request)
+    {
+        $request->session()->flush();
+        return redirect('/');
     }
 }
