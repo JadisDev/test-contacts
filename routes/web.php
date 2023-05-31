a8c59bd8df4090918b222f9579199c02a0910fda
Jadis Jale <jadisjale@gmail.com>
2023-05-31 16:36:50 -0300

    refactor: name funcations

--- routes/web.php
+++ routes/web.php
@@ -15,11 +15,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-
 //view
 Route::get('/', [UserController::class, 'index']);
 
@@ -30,7 +25,7 @@ Route::post('/auth', [UserController::class, 'auth'])->name("auth.user");
 Route::get('/logout', [UserController::class, 'logout'])->name("auth.logout");
 
 Route::middleware('auth.session')->group(function () {
-    Route::get('/contact', [ContactController::class, 'newContactView']);
+    Route::get('/contact', [ContactController::class, 'formContactView']);
     Route::get('/contact/{id}', [ContactController::class, 'updateView']);
     Route::get('/contact/detail/{id}', [ContactController::class, 'showView']);
     Route::post('/contact', [ContactController::class, 'store'])->name("contact.store");
